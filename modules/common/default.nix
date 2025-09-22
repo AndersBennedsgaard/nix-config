5414d46248f051666ded35521dbd6bd655d5fd15
Anders Bjørnkjær Bennedsgaard <abbennedsgaard@gmail.com>
2025-09-22 18:22:38 +0200

    feat: automatic garbage-collection

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -22,6 +22,14 @@
   };
 
   nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+  nix.optimise = {
+    automatic = true;
+  };
 
   time.timeZone = vars.system.timeZone;
 
